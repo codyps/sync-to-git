187add2924aaf09c3fa05a279a7cd77c61837a91
Cody P Schafer <cody@linux.vnet.ibm.com>
2013-04-25 19:23:08 -0500

    git support.

--- sync.sh
+++ sync.sh
@@ -4,5 +4,10 @@ cd $(dirname $0)
 for i in svn/*; do
 	! [ -d $i ] && continue
 
-	{ echo +++++; date; cd $i && git svn fetch && git push origin && git gc --aggressive --quiet; } >>logs/$i.log 2>&1
+	{ echo +++++; date; cd $i && git svn fetch && git push origin --mirror && git gc --aggressive --quiet; } >>logs/$i.log 2>&1
+done
+
+for i in git/*; do
+	! [ -d $i ] && continue
+	{ echo +++++; date; cd $i && git fetch && git push mirror --mirror && git gc --aggressive --quiet; } >>logs/$i.log 2>&1
 done
