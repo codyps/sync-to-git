49fcba3c61ded40604de7de590d36fec8ddfbe51
Cody P Schafer <cody@linux.vnet.ibm.com>
2013-05-08 18:04:20 -0500

    boop

--- sync.sh
+++ sync.sh
@@ -1,13 +1,29 @@
 #! /bin/sh
 
+if [ -z "$DEBUG" ]; then
+	DEBUG=0;
+fi
+
+pr () {
+	l=$1; shift
+	if [ "$l" -lt "$DEBUG" ]; then
+		echo "$@"
+	fi
+}
+
 cd $(dirname $0)
 for i in svn/*; do
-	! [ -d $i ] && continue
+	if ! [ -d $i ]; then
+		pr 1 "skipping $i"
+		continue
+	fi
+	pr 1 "processing $i"
 
-	{ echo +++++ $i ; date; cd $i && git svn fetch && git push origin --mirror && git gc --aggressive --quiet; } >>logs/$i.log 2>&1
+	( echo +++++ $i ; date; cd $i && git svn fetch && git push origin --mirror; ) >>logs/$i.log 2>&1
 done
 
 for i in git/*; do
 	! [ -d $i ] && continue
-	{ echo +++++ $i ; date; cd $i && git fetch && git push mirror --mirror && git gc --aggressive --quiet; } >>logs/$i.log 2>&1
+	pr 1 "processing $i"
+	( echo +++++ $i ; date; cd $i && git fetch && git push mirror --mirror; ) >>logs/$i.log 2>&1
 done
